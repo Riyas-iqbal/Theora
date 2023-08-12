e1818fb24ea70a1c93c6ddfa505b3b47aa95d330
Riyas Iqbal <riyasiqbal10@gmail.com>
2023-08-12 23:55:42 +0530

    TWA-5 GIthub Client setup

--- client/src/components/user/GoogleSignIn.jsx
+++ client/src/components/user/GoogleSignIn.jsx
@@ -27,7 +27,7 @@ function GoogleSignIn({ handleSignInSuccess }) {
   return (
     <button
       type="button"
-      className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
+      className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50  rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
       onClick={initializeGoogleSignIn}
     >
       <svg className="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
